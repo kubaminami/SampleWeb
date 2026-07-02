e6b8b98eff739c29889c02e851c68f3f512f7d88
unknown <kuba@Amity20221001.svan.local>
2026-07-02 14:43:24 +0900

    use bootJar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,27 @@ VALUES ('Sports');
 
 INSERT INTO category(name)
 VALUES ('Foods');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品A', 1000, '商品Aの説明です。', 'Books', '/images/omake_zasshi_otona.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品B', 2000, '商品Bの説明です。', 'Books', '/images/omake_zasshi_kodomo.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品C', 3000, '商品Cの説明です。', 'furniture', '/images/game_gaming_chair.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品D', 4000, '商品Dの説明です。', 'furniture', '/images/work_desk.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品E', 5000, '商品Eの説明です。', 'furniture', '/images/kagu_rocking_chair.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品F', 6000, '商品Fの説明です。', 'Books', '/images/entertainment_comic.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品G', 7000, '商品Gの説明です。', 'Sports', '/images/sports_badminton_shuttle.png');
+
+INSERT INTO product(name, price, description, category, img)
+VALUES ('商品H', 8000, '商品Hの説明です。', 'Sports', '/images/stopwatch.png');
