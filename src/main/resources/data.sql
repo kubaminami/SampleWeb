61c64399e65d8233d145450918570b33e536cd73
unknown <kuba@Amity20221001.svan.local>
2026-07-03 14:38:07 +0900

    use h2 on render

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,13 +27,13 @@ INSERT INTO product(name, price, description, category, img)
 VALUES ('商品B', 2000, '商品Bの説明です。', 'Books', '/images/omake_zasshi_kodomo.png');
 
 INSERT INTO product(name, price, description, category, img)
-VALUES ('商品C', 3000, '商品Cの説明です。', 'furniture', '/images/game_gaming_chair.png');
+VALUES ('商品C', 3000, '商品Cの説明です。', 'Furniture', '/images/game_gaming_chair.png');
 
 INSERT INTO product(name, price, description, category, img)
-VALUES ('商品D', 4000, '商品Dの説明です。', 'furniture', '/images/work_desk.png');
+VALUES ('商品D', 4000, '商品Dの説明です。', 'Furniture', '/images/work_desk.png');
 
 INSERT INTO product(name, price, description, category, img)
-VALUES ('商品E', 5000, '商品Eの説明です。', 'furniture', '/images/kagu_rocking_chair.png');
+VALUES ('商品E', 5000, '商品Eの説明です。', 'Furniture', '/images/kagu_rocking_chair.png');
 
 INSERT INTO product(name, price, description, category, img)
 VALUES ('商品F', 6000, '商品Fの説明です。', 'Books', '/images/entertainment_comic.png');
